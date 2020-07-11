7c5fe569dd1c72f306a08f5f5f3eb757f183bb04
Dianamarlen12 <50559771+Dianamarlen12@users.noreply.github.com>
2020-07-11 02:00:45 -0500

    Correcto

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\52775\AndroidStudioProjects\notificaciones"
+export "FLUTTER_APPLICATION_PATH=C:\Users\52775\AndroidStudioProjects\notificacioness"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
